7bd80d6286116bca003b3836eff4c71038944d00
guotao <guotao.chen@outlook.com>
2018-12-21 01:52:13 +0800

    在前端系统模板集成商业控件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { products } from './products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'web-app';
+  gridData: any[] = products;
+  items = [
+    {
+      title: 'Profile',
+      expanded: true,
+      children: [
+        {
+          title: 'Change Password',
+          link: [], // goes into angular `routerLink`
+        },
+        {
+          title: 'Privacy Policy',
+          ulr: '#', // goes directly into `href` attribute
+        },
+        {
+          title: 'Logout',
+          link: [],
+        },
+      ],
+    },
+    {
+      title: 'Shopping Bag',
+      link: [],
+    },
+    {
+      title: 'Orders',
+      link: [],
+    },
+  ];
+  onButtonClick() {
+    this.title = 'Hello from Kendo UI!';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbSidebarService, NbMenuModule, NbMenuService } from '@nebular/theme';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { NbMenuInternalService } from '@nebular/theme/components/menu/menu.service';
+
+
 
 @NgModule({
   declarations: [
@@ -15,9 +20,17 @@ import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'corporate' }),
-    NbLayoutModule
+    NbLayoutModule,
+    NbSidebarModule,
+    ButtonsModule,
+    GridModule,
+    NbMenuModule
   ],
-  providers: [],
+  providers: [
+    NbSidebarService,
+    NbMenuService,
+    NbMenuInternalService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
